601b4f2a06e667d37353c4921371b08f12660291
Bekhruz Akhmadjonov <behruzahmadjanov@icloud.com>
2024-12-24 02:48:06 +0500

    Update App.js
    Link to Navbar and Hero changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import React from 'react';
-import Navbar from '/Users/user/Documents/GitHub/react-shirin/src/components/navbar.jsx';
-import Hero from '/Users/user/Documents/GitHub/react-shirin/src/components/hero.jsx';
+import Navbar from '../src/components/navbar'
+import Hero from '../src/components/hero'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-
     </>
   );
 }
